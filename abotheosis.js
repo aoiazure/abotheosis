24b142809da945e67b153be64c789b0f088e27c4
Norapat Rerngkasetkig <papyrogaming@gmail.com>
2022-02-18 02:24:28 -0500

    Let it grab everything.

--- abotheosis.js
+++ abotheosis.js
@@ -2,10 +2,7 @@
 const { Client, Intents, Guild, MessageAttachment } = require('discord.js');
 // const { DISCORD_TOKEN, TO_PARSE_ID } = require("./config.json");
 
-// MD to PDF
-const fs = require('fs');
-const { Readable } = require('stream');
-const markdownpdf = require("markdown-pdf");
+const fetchAll = require('discord-fetch-all');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -15,35 +12,32 @@ client.once('ready', () => {
 	console.log(`Ready! Logged in as ${client.user.tag}`);
 });
 
-const parse_channel = (interaction, interactionId, parse_id) => {
+async function parse_channel (interaction, interactionId, parse_id) {
 	
 	const channel = client.channels.cache.get(parse_id);
 	const message_channel = client.channels.cache.get(interactionId);
 	
 	let text = "";
-	
+
 	const parse_text = (author, content) => {
 		text += `# ${author}:\n` + `${content}\n\n`;
 	}
-	channel.messages.fetch({ limit:99999 }).then(messages => { // limit: 100
-		messages = messages.reverse();
-		interaction.reply({ content:`Parsing ${messages.size} in this channel.`, ephemeral:true });
-		//Iterate through the messages here with the variable "messages".
-		messages.forEach(message => parse_text(message.author.username, message.content))
-		
-		//// Still have access to all of text
-		const m = new MessageAttachment( Buffer.from(text, 'utf-8'), `${new Date().toLocaleString()}-text.md` );
 
-		// send message
-		message_channel.send({
-				content:`Save the Markdown file (.md) and use https://stackedit.io/app# to prettify it.`,
-				files: [
-					m // file attachment
-				],
-			})
-			.then(console.log)
-			.catch(console.error)
-	})
+	const allMessages = await fetchAll.messages(channel, { reverseArray: true })
+	allMessages.forEach(message => parse_text(message.author.username, message.content))
+		
+	//// Still have access to all of text
+	const m = new MessageAttachment( Buffer.from(text, 'utf-8'), `${new Date().toLocaleString()}-text.md` );
+
+	// send message
+	message_channel.send({
+			content:`Save the Markdown file (.md) and use https://stackedit.io/app# to prettify it.`,
+			files: [
+				m // file attachment
+			],
+		})
+		.then(console.log)
+		.catch(console.error)
 }
 
 // Work with commands
@@ -54,11 +48,8 @@ client.on('interactionCreate', async interaction => {
 
 	switch(commandName) {
 		case "parse":
-			parse_channel(interaction, interaction.channelId, process.env.TO_PARSE_ID); // #testing
-			break;
-		case "kill":
-			await console.log('Good-bye, cruel world.');
-			await client.destroy();
+			await interaction.deferReply();
+			parse_channel(interaction, interaction.channelId, TO_PARSE_ID); // #testing
 			break;
 	}
 });
